e7e23e357aecfcf38057007d2e012190397b0dc9
milhani <haninamila@mail.ru>
2024-06-17 10:56:15 +0300

    Initial Commit

---
+++ TodoItem (YSHMR)/TodoItem__YSHMR_App.swift
@@ -0,0 +1,17 @@
+//
+//  TodoItem__YSHMR_App.swift
+//  TodoItem (YSHMR)
+//
+//  Created by Людмила Ханина on 17.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct TodoItem__YSHMR_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
